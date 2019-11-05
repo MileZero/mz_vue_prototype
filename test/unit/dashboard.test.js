b674f81c0f18f442a336182ad09e653c9e72b2b1
apoorvajain <apoorva@milezero.com>
2019-11-05 13:01:58 +0530

    Fixing vue dependency issue for unit test case

---
+++ test/unit/dashboard.test.js
@@ -0,0 +1,14 @@
+import dashboard from '@/components/Dashboard.vue';
+import { mount, shallowMount } from '@vue/test-utils'
+
+describe('Dashboard.vue', () => {
+    
+    it('renders a vue instance', () => {
+        expect(shallowMount(dashboard).isVueInstance()).toBe(true);
+      })
+
+      const wrapper = mount(dashboard)
+      it('has an h2', ()=>{
+        expect(wrapper.contains('h2')).toBe(true)
+      })
+})
