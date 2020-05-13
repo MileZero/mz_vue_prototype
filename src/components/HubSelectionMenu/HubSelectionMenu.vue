55453d2a0c21b261ca5737a8a2afbaab2c8d786f
rudydimacali <rudydimacali@gmail.com>
2020-05-12 18:29:20 -0700

    Add datetime parsing.

--- src/components/HubSelectionMenu/HubSelectionMenu.vue
+++ src/components/HubSelectionMenu/HubSelectionMenu.vue
@@ -1,28 +1,37 @@
 <template>
   <div class="HubSelectionMenu" v-on:click="dropdown">
     <div class="HubSelectionMenuContent" v-if="this.$auth.org.facilities">
-      <div class="HubInfo">{{this.$auth.org.facilities[currentHubIndex].label}}</div>
-        <transition name="HubOptions">
-          <div v-if="dropdownSelected" v-on:click.stop class="HubOptionsList">
-            <div class="MenuOptionsListItem HubListItem"
-                v-for="(hub, index) in this.$auth.org.facilities"
-                :key="hub.id"
-                v-bind:class="index === currentHubIndex ? 'SelectedHub' : ''"
-                v-on:click="setHub(index)">
-              {{hub.label}}
-            </div>
+      <div class="HubInfo">{{this.$auth.org.facilities[currentHubIndex].label}}
+      </div>
+      <div class="DateTime">
+        <div>{{currentTime}}</div>
+        <div>{{currentDate}}</div>
+      </div>
+      <transition name="HubOptions">
+        <div v-if="dropdownSelected" v-on:click.stop class="HubOptionsList">
+          <div class="MenuOptionsListItem HubListItem"
+              v-for="(hub, index) in this.$auth.org.facilities"
+              :key="hub.id"
+              v-bind:class="index === currentHubIndex ? 'SelectedHub' : ''"
+              v-on:click="setHub(index)">
+            {{hub.label}}
           </div>
-        </transition>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
+const moment = require('moment-timezone');
+
 export default {
   data() {
     return {
       currentHubIndex: 0,
       dropdownSelected: false,
+      currentDate: moment().format('dddd, MMMM DD'),
+      currentTime: moment().format('hh:mm:ss A (z)'),
     };
   },
   props: {
@@ -30,11 +39,21 @@ export default {
   methods: {
     dropdown() {
       this.dropdownSelected = !this.dropdownSelected;
+      console.log(this.$auth.org);
     },
     setHub(hubIndex) {
       this.currentHubIndex = hubIndex;
+      this.setDateTime();
       this.dropdownSelected = false;
     },
+    setDateTime() {
+      this.currentDate = moment.tz(this.$auth.org.facilities[this.currentHubIndex].location.timezone).format('dddd, MMMM DD');
+      this.currentTime = moment.tz(this.$auth.org.facilities[this.currentHubIndex].location.timezone).format('hh:mm:ss A (z)');
+    },
+  },
+  created() {
+    this.setDateTime();
+    setInterval(this.setDateTime, 1000);
   },
 };
 </script>
